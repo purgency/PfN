fc4ccef93db3d8ced936a68383df1f3165f5cbd0
purgency <purgency@users.noreply.github.com>
2016-06-06 20:31:56 +0200

    Update revcomp.c

--- revcomp.c
+++ revcomp.c
@@ -36,14 +36,14 @@ char compl(char x)
 int main(int argc, char **argv){
 
 	//printf("%lu",file_size("ecoli.seq"));
-	unsigned long lon = file_size("ecoli.seq");
+	unsigned long lon = file_size(argv[1]);
 
 	char *buff = malloc(lon*sizeof(char));
 
 
-	FILE *fp = fopen("ecoli.seq", "r+");
+	FILE *fp = fopen(argv[1], "r+");
 
-	FILE *fo = fopen("ecoli.seq.rc", "w+");
+	FILE *fo = fopen(strcat(argv[1],".rc"), "w+");
 
 	fread(buff, sizeof(char),lon,fp);
 
@@ -58,18 +58,32 @@ int main(int argc, char **argv){
 
     printf("%d", rowlength);
 
-	for(i = lon; i >= 0; i--)
+	for(i = lon-1; i >= 0; i--)
     {
-        if(j != rowlength)
+        if(j != rowlength && buff[i] != '\n')
         {
 
             char comp = compl(buff[i]);
             fputc(comp,fo);
+            j++;
         }
-        else{
-
+        else if(buff[i] != '\n'){
+            fputs("\n",fo);
+            j = 0;
+//            int k = i;
+//            int l = 0;
+//            while(buff[k] != '\n')
+//            {
+//                k++;
+//                l++;
+//            }
+//            rowlength = l;
+            i++;
         }
     }
+
+    fclose(fo);
+    fclose(fp);
 
 	return 0;
 }
